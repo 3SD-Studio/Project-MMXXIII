28c40989cc0d109e24a9122dddab631159cef381
Michał Pilch <mp300455@student.polsl.pl>
2023-02-17 00:46:38 +0100

    Improved multiplayer capabilities, and game logic

--- GamesLogic/TicTacToe.cs
+++ GamesLogic/TicTacToe.cs
@@ -1,68 +1,79 @@
 ﻿using System.Data.SqlTypes;
 using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Project_MMXXIII.GamesLogic {
     public class TicTacToe {
         static char[,] table = new char[3, 3];
         static bool turn = true;
+        static Thread? notifyThread;
+        static bool finished = false;
+        static char symbolWin = '\0';
 
-        public static async Task Update(WebSocket webSocket) {
+        public static async Task Echo(WebSocket webSocket) {
+            
+            // Thread notifying players about changes in game status. 
+            notifyThread = new Thread(async () => {
+                while (finished != true) {
+                    await Notify(webSocket);
+                    Thread.Sleep(100);
+                }
+                await Notify(webSocket);
+
+                var winningMessage = Encoding.Default.GetBytes($"{symbolWin} win.");
+                await webSocket.SendAsync(
+                    new ArraySegment<byte>(winningMessage, 0, winningMessage.Length),
+                    WebSocketMessageType.Text,
+                    WebSocketMessageFlags.EndOfMessage,
+                    CancellationToken.None
+                );
+            });
+            notifyThread.Start();
+
+            // Buffer needed for websocet.ReciveAsync method, used for storing data. 
             var buffer = new byte[1024 * 4];
+
+            // Data fri
             var receiveResult = await webSocket.ReceiveAsync(
-                new ArraySegment<byte>(buffer), CancellationToken.None);
+                new ArraySegment<byte>(buffer), CancellationToken.None
+            );
 
 
-            var response = System.Text.Encoding.Default.GetBytes("Something went wrong...");
-            if (receiveResult.MessageType == WebSocketMessageType.Text)
-            {
-                var str = System.Text.Encoding.Default.GetString(buffer, 0, receiveResult.Count);
+            var response = Encoding.Default.GetBytes("Something went wrong...");
+            if (receiveResult.MessageType == WebSocketMessageType.Text) {
+                var str = Encoding.Default.GetString(buffer, 0, receiveResult.Count);
                 var temp = turn ? 'x' : 'o';
                 turn = !turn;
 
-                table[int.Parse(str.Substring(1, 1)), int.Parse(str.Substring(5, 1))] = temp;
-
-                
-                
-                str += $" {temp}";
-
-                response = System.Text.Encoding.Default.GetBytes(str);
+                table[int.Parse(str.Substring(1, 1)) - 1, 
+                    int.Parse(str.Substring(5, 1)) - 1] = temp;
             }
 
-
-            while (!receiveResult.CloseStatus.HasValue)
-            {
-                await webSocket.SendAsync(
-                    new ArraySegment<byte>(response, 0, response.Length),
-                    receiveResult.MessageType,
-                    receiveResult.EndOfMessage,
-                    CancellationToken.None
-                );
-
+            while (!receiveResult.CloseStatus.HasValue) {
                 receiveResult = await webSocket.ReceiveAsync(
                     new ArraySegment<byte>(buffer), CancellationToken.None
                 );
 
-                response = System.Text.Encoding.Default.GetBytes("Something went wrong...");
+                response = Encoding.Default.GetBytes("Something went wrong...");
                 if (receiveResult.MessageType == WebSocketMessageType.Text)
                 {
-                    var str = System.Text.Encoding.Default.GetString(buffer, 0, receiveResult.Count);
+                    var str = Encoding.Default.GetString(buffer, 0, receiveResult.Count);
                     var temp = turn ? 'x' : 'o';
                     turn = !turn;
 
-                    table[int.Parse(str.Substring(1, 1)) - 1, int.Parse(str.Substring(5, 1)) - 1] = temp;
+                    var xIndex = int.Parse(str.Substring(1, 1)) - 1;
+                    var yIndex = int.Parse(str.Substring(5, 1)) - 1;
 
-                    var result = "";
-                    for (int i = 0; i < 3; i++) { 
-                        for (int j = 0; j < 3; j++) {
-                            result += $"x{i}, y{j} {table[i, j]} ;";
+                    if (table[xIndex, yIndex] == '\x00') {
+                        table[xIndex, yIndex] = temp;
+
+                        char symbol = ' ';
+                        if (Check(table, ref symbol) && !finished) {
+                            finished = true;
+                            symbolWin = symbol;
                         }
                     }
-
-
-                    str += $" {temp}";
-                    Console.WriteLine(result);
-
-                    response = System.Text.Encoding.Default.GetBytes(result);
                 }
             }
 
@@ -72,5 +83,73 @@ namespace Project_MMXXIII.GamesLogic {
                 CancellationToken.None
             );
         }
-    }
+
+
+        public static async Task Notify(WebSocket webSocket) {
+            var result = "";
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    result += $"x{i}, y{j} {table[i, j]} ;";
+                }
+            }
+
+            var response = Encoding.Default.GetBytes(result);
+
+            await webSocket.SendAsync(
+                new ArraySegment<byte>(response, 0, response.Length),
+                WebSocketMessageType.Text,
+                WebSocketMessageFlags.EndOfMessage,
+                CancellationToken.None
+            );
+        }
+
+
+        private static bool Check(char[,] field, ref char symbol) {
+            //Checking rows
+            if ((table[0, 0] == table[1, 0]) && (table[1, 0] == table[2, 0]) && table[0, 0] != 0) {
+                symbol = table[0, 0];
+                return true;
+            }
+            else if((table[0, 1] == table[1, 1]) && (table[1, 1] == table[2, 1]) && table[0, 1] != 0)
+            {
+                symbol = table[0, 1];
+                return true;
+            }
+            else if ((table[0, 2] == table[1, 2]) && (table[1, 2] == table[2, 2]) && table[0, 2] != 0)
+            {
+                symbol = table[0, 2];
+                return true;
+            }
+            //Checking columns
+            else if ((table[0, 0] == table[0, 1]) && (table[0, 1] == table[0, 2]) && table[0, 0] != 0)
+            {
+                symbol = table[0, 0];
+                return true;
+            }
+            else if ((table[1, 0] == table[1, 1]) && (table[1, 1] == table[1, 2]) && table[1, 0] != 0)
+            {
+                symbol = table[1, 0];
+                return true;
+            }
+            else if ((table[2, 0] == table[2, 1]) && (table[2, 1] == table[2, 2]) && table[2, 0] != 0)
+            {
+                symbol = table[2, 0];
+                return true;
+            }
+            //Checking diagonals
+            else if ((table[0, 0] == table[1, 1]) && (table[1, 1] == table[2, 2]) && table[0, 0] != 0)
+            {
+                symbol = table[0, 0];
+                return true;
+            }
+            else if ((table[2, 0] == table[1, 1]) && (table[1, 1] == table[0, 2]) && table[2, 0] != 0)
+            {
+                symbol = table[2, 0];
+                return true;
+            }
+            //Checked ✔️
+            else
+                return false;
+        }
+    }        
 }
--- Program.cs
+++ Program.cs
@@ -21,12 +21,16 @@ var webSocketsOptions = new WebSocketOptions
 
 app.UseWebSockets(webSocketsOptions);
 
+
+// Handles websocket requests.
 app.Use(async (context, next) => {
     if (context.Request.Path == "/ws") {
         if (context.WebSockets.IsWebSocketRequest) {
             using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-            await Project_MMXXIII.GamesLogic.TicTacToe.Update(webSocket);
-            //await Project_MMXXIII.GamesLogic.TicTacToe.Notificate(webSocket);
+
+            // Function that handle network ang game logic for tic tac toe, 
+            // Class located in GamesLogic folder
+            await Project_MMXXIII.GamesLogic.TicTacToe.Echo(webSocket);
         } 
         else {
             context.Response.StatusCode = StatusCodes.Status400BadRequest;
@@ -48,53 +52,3 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
-
-
-var ticTacToeField = new char[3, 3];
-
-
-static async Task Echo(WebSocket webSocket) {
-    var buffer = new byte[1024 * 4];
-    var receiveResult = await webSocket.ReceiveAsync(
-        new ArraySegment<byte>(buffer), CancellationToken.None);
-
-    var response = System.Text.Encoding.Default.GetBytes("This is not a text input.");
-    if (receiveResult.MessageType == WebSocketMessageType.Text) {
-        var str = System.Text.Encoding.Default.GetString(buffer, 0, receiveResult.Count);
-        Console.WriteLine(str);
-        str += " - IT IS SERVER RESPONSE";
-        response = System.Text.Encoding.Default.GetBytes(str);
-    }
-
-    
-    while (!receiveResult.CloseStatus.HasValue) {
-        await webSocket.SendAsync(
-            new ArraySegment<byte>(response, 0, response.Length),
-            receiveResult.MessageType,
-            receiveResult.EndOfMessage,
-            CancellationToken.None
-        );
-
-        receiveResult = await webSocket.ReceiveAsync(
-            new ArraySegment<byte>(buffer), CancellationToken.None
-        );
-        
-        response = System.Text.Encoding.Default.GetBytes("This is not a text input.");
-        if (receiveResult.MessageType == WebSocketMessageType.Text) {
-            var str = System.Text.Encoding.Default.GetString(buffer, 0, receiveResult.Count);
-            Console.WriteLine(str);
-            str += " - IT IS SERVER RESPONSE";
-            response = System.Text.Encoding.Default.GetBytes(str);
-        }
-    }
-
-    await webSocket.CloseAsync(
-        receiveResult.CloseStatus.Value,
-        receiveResult.CloseStatusDescription,
-        CancellationToken.None
-    );
-}
-
-
-
-
