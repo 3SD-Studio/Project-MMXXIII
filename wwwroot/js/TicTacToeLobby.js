df9b589a7d6704fd65b8e6ee33bed79057fcc7f9
Dave <dj300758@student.polsl.pl>
2023-02-21 14:25:51 +0100

    Lobby create-button functionality.

--- wwwroot/js/TicTacToeLobby.js
+++ wwwroot/js/TicTacToeLobby.js
@@ -1,1 +1,23 @@
-﻿
+﻿let create_button = document.getElementById("create-button");
+
+create_button.addEventListener("click", function () { newLobby(); });
+
+function newLobby() {
+    let newLink = document.createElement('li');
+    let a = document.createElement("a");
+    a.href = "~/tictactoegame";
+
+    let div = document.createElement("div");
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    p1.innerHTML = "game_id";
+    p2.innerHTML = "player_amount";
+
+    div.appendChild(p1);
+    div.appendChild(p2);
+
+    a.appendChild(div);
+
+    newLink.appendChild(a);
+    document.getElementById("lobbies").appendChild(newLink);
+}
